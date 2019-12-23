5b0d69058eba6f85b0e4c6993353c9aeddd6bce9
Jack Pritchard <jack@noface.co.uk>
2019-12-23 17:35:54 +0000

    Add logout button for client

--- src/pages/account/index.jsx
+++ src/pages/account/index.jsx
@@ -1,11 +1,12 @@
 import React from "react"
-import { Formik } from "formik"
 
 import StyledAccount from "./account.styles"
 
+import Layout from "../../storybook/src/components/particles/layout"
+
 import Link from "../../storybook/src/components/atoms/link/link"
 
-import Layout from "../../storybook/src/components/particles/layout"
+import Logout from "../../storybook/src/components/molecules/logout/logout"
 
 export default props => {
   return (
@@ -14,88 +15,7 @@ export default props => {
         <div className="register__wrapper">
           <h1 className="h3">Register an account</h1>
           <Link href="/">Return to homepage</Link>
-          <Formik
-            initialValues={{
-              email: "",
-              firstName: "",
-              lastName: "",
-              password: "",
-            }}
-            validate={values => {
-              const errors = {}
-              if (!values.email) {
-                errors.email = "Required"
-              } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-              ) {
-                errors.email = "Invalid email address"
-              }
-              return errors
-            }}
-            validateOnBlur={false}
-            validateOnChange={false}
-            onSubmit={(values, { setSubmitting }) => {
-              // createAccount({ variables: { ...values } })
-              setSubmitting(false)
-            }}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              /* and other goodies */
-            }) => (
-              <form disabled={isSubmitting} onSubmit={handleSubmit}>
-                <label htmlFor="email">Email Address</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                />
-                {errors.email && touched.email && errors.email}
-                <label htmlFor="firstName">First Name</label>
-                <input
-                  type="text"
-                  id="firstName"
-                  name="firstName"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.firstName}
-                />
-                {errors.firstName && touched.firstName && errors.firstName}
-                <label htmlFor="lastName">Last Name</label>
-                <input
-                  type="text"
-                  id="lastName"
-                  name="lastName"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.lastName}
-                />
-                {errors.lastName && touched.lastName && errors.lastName}
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                />
-                {errors.password && touched.password && errors.password}
-                <button type="submit" disabled={isSubmitting}>
-                  Create Account
-                </button>
-              </form>
-            )}
-          </Formik>
+          <Logout />
         </div>
       </StyledAccount>
     </Layout>
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,122 @@
+import React from "react"
+import { Formik } from "formik"
+import { useMutation } from "@apollo/react-hooks"
+
+import StyledLogin from "./login.styles"
+
+import LOGIN_USER_MUTATION from "../../mutations/user/LOGIN_USER_MUTATION"
+
+import Layout from "../../storybook/src/components/particles/layout"
+
+import Link from "../../storybook/src/components/atoms/link/link"
+
+import ErrorMessage from "../../storybook/src/components/molecules/error-message/errorMessage"
+
+export default props => {
+  const hasToken = localStorage.getItem("authToken")
+  const [loginUser, { data, error, loading }] = useMutation(LOGIN_USER_MUTATION)
+
+  const processLogin = async values => {
+    const login = await loginUser({
+      variables: { clientMutationId: "test", ...values },
+    })
+
+    if (
+      !login ||
+      !login.data ||
+      !login.data.login ||
+      !login.data.login.authToken
+    )
+      return false
+
+    localStorage.setItem("authToken", login.data.login.authToken)
+  }
+
+  return (
+    <Layout {...props} cart={false} header={false} footer={false}>
+      <StyledLogin>
+        <div className="login__wrapper">
+          {hasToken && (
+            <>
+              <h1 className="h3">You're already logged in</h1>
+              <Link href="/">Return to homepage</Link>
+            </>
+          )}
+          {!hasToken && (
+            <>
+              <h1 className="h3">Login to your account</h1>
+              <Link href="/">Return to homepage</Link>
+              {error && error.message && (
+                <ErrorMessage message={error.message} />
+              )}
+              <Formik
+                initialValues={{
+                  password: "",
+                  username: "",
+                }}
+                validate={values => {
+                  const errors = {}
+                  if (!values.password) errors.password = "Required"
+                  if (!values.username) errors.username = "Required"
+                  return errors
+                }}
+                validateOnBlur={false}
+                validateOnChange={false}
+                onSubmit={async (values, { setSubmitting }) => {
+                  await processLogin(values)
+                  setSubmitting(false)
+                }}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                  /* and other goodies */
+                }) => (
+                  <form
+                    aria-busy={isSubmitting || loading}
+                    disabled={isSubmitting || loading}
+                    onSubmit={handleSubmit}
+                  >
+                    <label htmlFor="username">Username</label>
+                    <input
+                      type="text"
+                      id="username"
+                      name="username"
+                      autoFocus={true}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.username}
+                    />
+                    {errors.username && touched.username && (
+                      <ErrorMessage message={errors.username} />
+                    )}
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}
+                    />
+                    {errors.password && touched.password && (
+                      <ErrorMessage message={errors.password} />
+                    )}
+                    <button type="submit" disabled={isSubmitting || loading}>
+                      Log{loading ? `ging in` : `in`} to Account
+                    </button>
+                  </form>
+                )}
+              </Formik>
+            </>
+          )}
+        </div>
+      </StyledLogin>
+    </Layout>
+  )
+}
