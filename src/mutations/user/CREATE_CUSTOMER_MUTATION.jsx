0cc3da78fdf36fe19e309f343cb5e369b3b92d1f
Jack Pritchard <jack@noface.co.uk>
2019-12-27 10:18:40 +0000

    Update createUser mutation to registerUser

--- src/mutations/user/CREATE_CUSTOMER_MUTATION.jsx
+++ src/mutations/user/CREATE_CUSTOMER_MUTATION.jsx
@@ -1,31 +1,30 @@
-import gql from "graphql-tag"
+import gql from "graphql-tag";
 
 export const CREATE_CUSTOMER_ACCOUNT = gql`
-  mutation CREATE_CUSTOMER_ACCOUNT(
-    $clientMutationId: String!
-    $email: String!
-    $firstName: String
-    $lastName: String
-    $password: String!
-  ) {
-    createUser(
-      input: {
-        clientMutationId: $clientMutationId
-        username: $email
-        password: $password
-        nickname: $email
-        email: $email
-        roles: ["Customer"]
-        firstName: $firstName
-        lastName: $lastName
-      }
-    ) {
-      clientMutationId
-      user {
-        email
-      }
-    }
-  }
-`
+	mutation CREATE_CUSTOMER_ACCOUNT(
+		$clientMutationId: String!
+		$email: String!
+		$firstName: String
+		$lastName: String
+		$password: String!
+	) {
+		registerUser(
+			input: {
+				clientMutationId: $clientMutationId
+				email: $email
+				firstName: $firstName
+				lastName: $lastName
+				nickname: $email
+				password: $password
+				username: $email
+			}
+		) {
+			clientMutationId
+			user {
+				email
+			}
+		}
+	}
+`;
 
-export default CREATE_CUSTOMER_ACCOUNT
+export default CREATE_CUSTOMER_ACCOUNT;
