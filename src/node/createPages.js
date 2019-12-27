3ad6dd00da90fef0301512e24082876be0914138
Jack Pritchard <jack@noface.co.uk>
2019-12-27 10:00:29 +0000

    Add hardcoded homepage

--- src/node/createPages.js
+++ src/node/createPages.js
@@ -1,7 +1,7 @@
-const path = require(`path`)
+const path = require(`path`);
 
 module.exports = async ({ actions, graphql }) => {
-  const GET_PAGES = `
+	const GET_PAGES = `
   query GET_PAGES($first:Int){
     wordpress {
       pages( first: $first ) {
@@ -13,30 +13,31 @@ module.exports = async ({ actions, graphql }) => {
       }
     }
   }
-  `
-  const { createPage } = actions
+  `;
+	const { createPage } = actions;
 
-  const fetchPages = async variables =>
-    await graphql(GET_PAGES, variables).then(({ data }) => {
-      return data.wordpress.pages.nodes
-    })
+	const fetchPages = async variables =>
+		await graphql(GET_PAGES, variables).then(({ data }) => {
+			return data.wordpress.pages.nodes;
+		});
 
-  await fetchPages({ first: 500 }).then(allPages => {
-    allPages.map(page => {
-      console.log(`Creating page: ${page.uri}`)
+	await fetchPages({ first: 500 }).then(allPages => {
+		allPages.map(page => {
+			console.log(`Creating page: ${page.uri}`);
 
-      const { isFrontPage } = page
-      const uri = isFrontPage ? `/` : `/${page.uri}`
+			const { isFrontPage } = page;
+			if (isFrontPage) return null;
+			const uri = isFrontPage ? `/` : `/${page.uri}`;
 
-      actions.createPage({
-        path: uri,
-        component: path.resolve(
-          `./src/storybook/src/components/templates/page.jsx`
-        ),
-        context: {
-          ...page,
-        },
-      })
-    })
-  })
-}
+			actions.createPage({
+				path: uri,
+				component: path.resolve(
+					`./src/storybook/src/components/templates/page.jsx`
+				),
+				context: {
+					...page
+				}
+			});
+		});
+	});
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import staticData from "./static.json";
+
+import Banner from "../storybook/src/components/organisms/banner/banner";
+import Carousel from "../storybook/src/components/organisms/carousel/carousel";
+
+const { banner, carousel } = staticData;
+
+const Homepage = () => (
+	<>
+		<Banner {...banner} />
+		<Carousel {...carousel} />
+	</>
+);
+
+export default Homepage;
