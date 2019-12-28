3d242112ac9eb9132d2585bec9b1850ece9ef3df
Jack Pritchard <jack@noface.co.uk>
2019-12-28 12:57:20 +0000

    Reset WORDPRESS typeName

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,43 @@
 const activeEnv =
-  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || `development`;
+	process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || `development`;
 
 console.log(`Using environment config: "${activeEnv}"`);
 
 require(`dotenv`).config({
-  path: `.env.${activeEnv}`
+	path: `.env.${activeEnv}`
 });
 
 console.log(`Using wordpress source: ${process.env.GATSBY_WORDPRESS_API}`);
 
 module.exports = {
-  plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // Arbitrary name for the remote schema Query type
-        typeName: "WP",
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-        fieldName: "wp",
-        // Url to query from, this has to be static
-        url: "https://celticwordpress.co.uk/graphql"
-      }
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /\.svg$/
-        }
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `assets`,
-        path: `${__dirname}/src/`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
-  ]
+	plugins: [
+		{
+			resolve: "gatsby-source-graphql",
+			options: {
+				// Arbitrary name for the remote schema Query type
+				typeName: "WORDPRESS",
+				// Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+				fieldName: "wordpress",
+				// Url to query from, this has to be static
+				url: "https://celticwordpress.co.uk/graphql"
+			}
+		},
+		{
+			resolve: "gatsby-plugin-react-svg",
+			options: {
+				rule: {
+					include: /\.svg$/
+				}
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `assets`,
+				path: `${__dirname}/src/`
+			}
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`
+	]
 };
