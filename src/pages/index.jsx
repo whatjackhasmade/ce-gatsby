c8ba1eea4ec5c864c5dd6bdba0b5fbb9d6bcd5d9
Jack Pritchard <jack@noface.co.uk>
2019-12-28 12:57:09 +0000

    Fix slick bug

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 import staticData from "./static.json";
 
+import Layout from "../storybook/src/components/particles/layout";
+
 import Banner from "../storybook/src/components/organisms/banner/banner";
 import Carousel from "../storybook/src/components/organisms/carousel/carousel";
 
 const { banner, carousel } = staticData;
 
 const Homepage = () => (
-  <>
-    <Banner {...banner} />
-    <Carousel {...carousel} />
-  </>
+	<Layout>
+		<Banner {...banner} />
+		<Carousel {...carousel} />
+	</Layout>
 );
 
 export default Homepage;
