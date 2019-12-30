f3251be63cb3d60e57e9fc85c1c9ac8f8d34cb11
Jack Pritchard <jack@noface.co.uk>
2019-12-30 18:12:11 +0000

    Start insights page

--- src/gatsby/gatsbyWrapper.jsx
+++ src/gatsby/gatsbyWrapper.jsx
@@ -5,61 +5,61 @@ import useAllProducts from "./hooks/useAllProducts";
 import useHeaderMenu from "./hooks/useHeaderMenu";
 
 const PropProducer = props => {
-  let allMenus = useAllMenus();
-  allMenus = allMenus.map(menu => menu.node);
-  const menuHeader = useHeaderMenu();
-  let allProducts = useAllProducts();
-  allProducts = allProducts.map(prod => prod.node);
+	let allMenus = useAllMenus();
+	allMenus = allMenus.map(menu => menu.node);
+	const menuHeader = useHeaderMenu();
+	let allProducts = useAllProducts();
+	allProducts = allProducts.map(prod => prod.node);
 
-  let footerMenus = allMenus.filter(menu => menu.slug !== "header-menu");
-  footerMenus = footerMenus.map(menu => {
-    return {
-      items: menu.menuItems.edges.map(item => item.node),
-      title: menu.name
-    };
-  });
+	let footerMenus = allMenus.filter(menu => menu.slug !== "header-menu");
+	footerMenus = footerMenus.map(menu => {
+		return {
+			items: menu.menuItems.edges.map(item => item.node),
+			title: menu.name
+		};
+	});
 
-  let headerMenu;
-  headerMenu = menuHeader.map(node => node.node);
-  headerMenu = [
-    { items: headerMenu },
-    {
-      title: "account",
-      items: [
-        {
-          icon: null,
-          label: "Insights",
-          target: null,
-          url: "#"
-        },
-        {
-          icon: null,
-          label: "Account",
-          target: null,
-          url: "account"
-        },
-        {
-          icon: "user",
-          label: "User",
-          target: null,
-          url: "orders"
-        },
-        {
-          icon: "bag",
-          label: "Cart",
-          target: null,
-          url: "checkout"
-        }
-      ]
-    }
-  ];
+	let headerMenu;
+	headerMenu = menuHeader.map(node => node.node);
+	headerMenu = [
+		{ items: headerMenu },
+		{
+			title: "account",
+			items: [
+				{
+					icon: null,
+					label: "Insights",
+					target: null,
+					url: "/insights"
+				},
+				{
+					icon: null,
+					label: "Account",
+					target: null,
+					url: "/account"
+				},
+				{
+					icon: "user",
+					label: "User",
+					target: null,
+					url: "/login"
+				},
+				{
+					icon: "bag",
+					label: "Cart",
+					target: null,
+					url: "/checkout"
+				}
+			]
+		}
+	];
 
-  const newProps = {
-    ...props,
-    gatsbyContext: { allProducts, headerMenu, footerMenus }
-  };
+	const newProps = {
+		...props,
+		gatsbyContext: { allProducts, headerMenu, footerMenus }
+	};
 
-  return <>{React.cloneElement(props.children, newProps)}</>;
+	return <>{React.cloneElement(props.children, newProps)}</>;
 };
 
 export default PropProducer;
