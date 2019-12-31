1c26084c3eb1eaeb004ea2dd062f5eab20aa4e6c
Jack Pritchard <jack@noface.co.uk>
2019-12-31 12:17:28 +0000

    Add panels to homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,9 +8,10 @@ import Layout from "../storybook/src/components/particles/layout";
 
 import Banner from "../storybook/src/components/organisms/banner/banner";
 import Carousel from "../storybook/src/components/organisms/carousel/carousel";
+import Panels from "../storybook/src/components/organisms/panels/panels";
 import Slider from "../storybook/src/components/organisms/slider/slider";
 
-const { banner, carousel, slider } = staticData;
+const { banner, carousel, panels, slider } = staticData;
 
 const Homepage = props => {
 	const products = useAllProducts();
@@ -19,6 +20,7 @@ const Homepage = props => {
 	return (
 		<Layout {...props} headerVariant="fixedLight">
 			<Slider {...slider} />
+			<Panels {...panels} />
 			<Banner {...banner} />
 			<Carousel {...carousel} items={carouselProducts} type="product" />
 		</Layout>
