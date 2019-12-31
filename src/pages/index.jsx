10fd0c02ab92b4d142d1698f34a5f4171d0d858d
Jack Pritchard <jack@noface.co.uk>
2019-12-31 11:23:14 +0000

    Add slider to homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,17 +8,17 @@ import Layout from "../storybook/src/components/particles/layout";
 
 import Banner from "../storybook/src/components/organisms/banner/banner";
 import Carousel from "../storybook/src/components/organisms/carousel/carousel";
+import Slider from "../storybook/src/components/organisms/slider/slider";
 
-const { banner, carousel } = staticData;
+const { banner, carousel, slider } = staticData;
 
 const Homepage = props => {
 	const products = useAllProducts();
 	const carouselProducts = products.map(p => p.node);
 
-	console.log(products);
-
 	return (
 		<Layout {...props}>
+			<Slider {...slider} />
 			<Banner {...banner} />
 			<Carousel {...carousel} items={carouselProducts} type="product" />
 		</Layout>
